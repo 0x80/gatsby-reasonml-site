9f3365017ce8f68ca4ae4050d01b1ea8d3d171e2
Thijs Koerselman <thijskoerselman@gmail.com>
2020-08-08 14:58:50 +0200

    Fix graphql import

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,8 +2,11 @@ import './index.css'
 
 import IndexLayout from './IndexLayout.re'
 import React from 'react'
+import { graphql } from 'gatsby'
 
-export default ({ data, children }) => <IndexLayout data={data}>{children}</IndexLayout>
+export default ({ data, children }) => (
+  <IndexLayout data={data}>{children}</IndexLayout>
+)
 
 export const query = graphql`
   query SiteTitleQuery {
